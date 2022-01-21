aa51a486e4e5d6d2861fcaa216e6c79eee97e986
Hannah Park <70654324+hannahcode@users.noreply.github.com>
2022-01-20 19:46:36 -0500

    Update src/components/statline/statline.tsx

--- src/components/statline/statline.tsx
+++ src/components/statline/statline.tsx
@@ -9,7 +9,7 @@ export const StatLine = ({labels, values}: Props) => {
   return (
     <div className="flex justify-center m-1">
     {values.map((value,i ) => (
-       <div key={i} className="items-center justify-center m-1 w-1/4 bg-blue-300">
+       <div key={i} className="items-center justify-center m-1 w-1/4">
          <div className="text-3xl font-bold">{value} </div>
          <div className="text-sm">{labels[i]}</div>
        </div>
