b95e55c60175fa937a5f0732a7f50907084b1c5a
eggplants <w10776e8w@yahoo.co.jp>
2022-01-26 21:16:02 +0900

    fix: test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />)
   const linkElement = screen.getByText(/Not Wordle/)
   expect(linkElement).toBeInTheDocument()
