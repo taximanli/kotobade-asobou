fcf91e826513de313aa6c71bfb36484a15ef2141
eggplants <w10776e8w@yahoo.co.jp>
2022-01-26 20:57:29 +0900

    fix: syntax with `npm run fix`

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -4,7 +4,7 @@ import { solutionIndex } from './words'
 export const shareStatus = (guesses: string[]) => {
   navigator.clipboard.writeText(
     `Not Wordle ${solutionIndex} ${guesses.length}/6\n\n` +
-    generateEmojiGrid(guesses)
+      generateEmojiGrid(guesses)
   )
 }
 
