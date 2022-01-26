3478533926d2ea5c2c06bb7ddf3d15e0afaf12a9
Jacob Louis Hoover <postylem@users.noreply.github.com>
2022-01-26 18:10:42 -0500

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -21,7 +21,7 @@ export const getWordOfDay = () => {
   const nextday = (index + 1) * msInDay + epochMs
 
   return {
-    solution: WORDS[index % WORDS.length].toUpperCase()
+    solution: WORDS[index % WORDS.length].toUpperCase(),
     solutionIndex: index,
     tomorrow: nextday,
   }
