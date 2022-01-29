524c9dc0d2fac386eb6063fef06b09ac30a6c676
taximanli <sumomo_nagamoto@yahoo.com>
2022-01-30 09:35:58 +1100

    Fixed share for Colour Blind Mode and set no caching

--- main.g2617e32.js
+++ main.g2617e32.js
@@ -3099,12 +3099,12 @@
                                       switch (e) {
                                         case Ma:
                                           a = (function (e) {
-                                            return e ? "🟩" : "🟩";
+                                            return e ? "🟧" : "🟩";
                                           })(i);
                                           break;
                                         case Ia:
                                           a = (function (e) {
-                                            return e ? "🟨" : "🟨";
+                                            return e ? "🟦" : "🟨";
                                           })(i);
                                           break;
                                         case Oa:
