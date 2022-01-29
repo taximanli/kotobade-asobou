8996c68103dd24b5567af0394df05ee595fb7077
Alexandra Robison <alexandra@arobison.net>
2022-01-29 11:07:30 -0700

    run linter and mock window for jest

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -21,7 +21,8 @@ export const Key = ({
   const classes = classnames(
     'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
     {
-      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400': !status,
+      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
+        !status,
       'bg-slate-400 text-white': status === 'absent',
       'bg-green-500 hover:bg-green-600 active:bg-green-700 text-white':
         status === 'correct',
