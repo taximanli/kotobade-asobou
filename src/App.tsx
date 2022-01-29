c60138bfbbd8bf914a6d290ede77057f1256f21e
gbear605 <developer@vivissi.com>
2022-01-29 18:18:49 -0500

    Move game down so alert doesn't overlap

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,7 @@ function App() {
 
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div className="flex w-80 mx-auto items-center mb-8">
+      <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl grow font-bold">Not Wordle</h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
