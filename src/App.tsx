9dcd05c0e1ae0fc1d71efa6d930a3dfb2da6605a
gbear605 <developer@vivissi.com>
2022-01-29 18:40:35 -0500

    Apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,9 @@ function App() {
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold dark:text-white">{WORDLE_TITLE}</h1>
+        <h1 className="text-xl grow font-bold dark:text-white">
+          {WORDLE_TITLE}
+        </h1>
         <SunIcon
           className="h-6 w-6 cursor-pointer dark:stroke-white"
           onClick={() => handleDarkMode(!isDarkMode)}
