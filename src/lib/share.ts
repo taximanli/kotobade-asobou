59c3b0e99f682a39b4130fb7d4f598b0c48a9480
teddyh-io <hwangjinsong@gmail.com>
2022-02-07 00:30:39 -0500

    Reflect color reversal into the share sheet as well
    I noticed that you decided to change back the color scheme to reflect the original game, but forgot to change the share function back as well.

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -18,9 +18,9 @@ export const generateEmojiGrid = (guesses: string[]) => {
         .map((_, i) => {
           switch (status[i]) {
             case 'correct':
-              return '🟦'
+              return '🟩'
             case 'present':
-              return '🟧'
+              return '🟨'
             default:
               return '⬜'
           }
