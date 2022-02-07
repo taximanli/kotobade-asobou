738ebf7d5b8fc9fed246c84dc14e037a0cd3ab07
Chase Wackerfuss <cwackerfuss@gmail.com>
2022-02-07 00:33:03 -0500

    updated url in about modal

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,9 +9,10 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        This is an open source version of the word guessing game we all know and
+        love -{' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
           check out the code here
